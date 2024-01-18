17b448cf1efab2f992635c72c48a2630e1e88075
Alessandra Scarpellini <scaalessandra@icloud.com>
2024-01-18 17:43:45 +0100

    tailwind and react-daisy-ui installation and configuration

---
+++ projects-data.js
@@ -0,0 +1,49 @@
+const projects = [
+    {
+        id: 4,
+        projectName: "",
+        gitUrl: "https://github.com/italian-iranian-connection",
+        appUrl: "https://logstation.netlify.app",
+        description: "",
+        images: [""],
+        tech:[]
+    },
+    {
+        id: 4,
+        projectName: "Music-Event Platform",
+        gitUrl: "https://github.com/Ironic-event-creator/music-event-platform-project2",
+        appUrl: "https://music-event-platform.adaptable.app/",
+        description: `SSR Express application that display information about live music events in Istanbul. A registered user can create, update and delete. Or simply leave a comment for the events already posted.`,
+        images: [""],
+        tech:[]
+    },
+    {
+        id: 3,
+        projectName: "Shoot the Pigeon",
+        gitUrl: "https://github.com/Aleale81/Game-project1",
+        appUrl: "https://aleale81.github.io/Game-project1/",
+        description: `A "shoot 'em all" game built in vanilla javascript, html and css.
+
+        A perfect way to improve programmig fundamental skills like OOP, DOM manipulation, loops and more...`,
+        images: [""],
+        tech:[]
+    },
+    {
+        id: 2,
+        projectName: "Dictionary App",
+        gitUrl: "https://github.com/Aleale81/dictionary-project",
+        appUrl: "https://eager-heisenberg-9fdca9.netlify.app/",
+        description: `The second final project for the SheCodes React Workshop. This app uses data from two different APIs, the Free Dictionary to show the definition of the word you are looking for and the Pexels ones to display pictures related to the word searched`,
+        images: [""],
+        tech:[]
+    },
+    {
+        id: 1,
+        projectName: "Weather App",
+        gitUrl: "https://github.com/Aleale81/project-weather-app",
+        appUrl: "https://alessandra-weather-app.netlify.app/",
+        description: `One of the final projects of the SheCodes React Workshop. This React app shows live data from OpenWeather API and displays nice animated icons using React Animated Weather customizable component.`,
+        images: [""],
+        tech:[]
+    }, 
+]
