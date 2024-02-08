f2361eaf8469cf1950bc3180059d20f72e3eb5dc
Alessandra Scarpellini <scaalessandra@icloud.com>
2024-02-08 17:28:06 +0100

    create new Experience component to display details of single experience

---
+++ src/components/Experience.jsx
@@ -0,0 +1,41 @@
+import { Button, ChatBubble } from "react-daisyui";
+
+const Experience = ({ experience }) => {
+  const bubblePosition = (id) => {
+    if (id % 2 === 0) {
+      return "true";
+    }
+  };
+
+  return (
+    <ChatBubble end={bubblePosition(experience.id)} className="m-4 md:mt-10 md:mb-10">
+      <ChatBubble.Header className="text-secondary">
+        <i className="opacity-70">{experience.period} - </i> {experience.position}
+        <br />
+      </ChatBubble.Header>
+      <ChatBubble.Message className="shadow-md shadow-accent m-3">
+        {experience.description}
+      </ChatBubble.Message>
+      <ChatBubble.Footer className="opacity-1">
+        Learnings:{" "}
+        {experience.learnings.map((learning, index) => {
+          return (
+            <Button
+              key={index}
+              className="m-0.5 cursor-default"
+              color="primary"
+              size="xs"
+              glass
+              variant="outline"
+              active="true"
+            >
+              {learning}
+            </Button>
+          );
+        })}
+      </ChatBubble.Footer>
+    </ChatBubble>
+  );
+};
+
+export default Experience;
